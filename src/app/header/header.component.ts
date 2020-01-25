b411f8803c1eddcd20032118be87b8de10b8bfa9
Marwa Gaser <marwagaser@gmail.com>
2020-01-25 21:40:21 +0200

    added tab navigation and idea tab

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { NgAnimateScrollService } from "ng-animate-scroll";
 
 @Component({
   selector: "app-header",
@@ -7,7 +8,11 @@ import { Component, OnInit } from "@angular/core";
 })
 export class HeaderComponent implements OnInit {
   imgurl = "../assets/3d.png";
-  constructor() {}
 
+  constructor(private animateScrollService: NgAnimateScrollService) {}
   ngOnInit() {}
+
+  scroll(id: string) {
+    this.animateScrollService.scrollToElement(id, 1000);
+  }
 }
--- src/app/idea-page/idea-page.component.ts
+++ src/app/idea-page/idea-page.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { DomSanitizer } from "@angular/platform-browser";
 @Component({
-  selector: 'app-idea-page',
-  templateUrl: './idea-page.component.html',
-  styleUrls: ['./idea-page.component.css']
+  selector: "app-idea-page",
+  templateUrl: "./idea-page.component.html",
+  styleUrls: ["./idea-page.component.css"]
 })
 export class IdeaPageComponent implements OnInit {
+  videoURL = "https://www.youtube.com/embed/Ri4RNVD3ifI";
+  constructor(private sanitizer: DomSanitizer) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  fetchURL() {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(
+      "https://www.youtube.com/embed/Ri4RNVD3ifI"
+    );
   }
-
 }
