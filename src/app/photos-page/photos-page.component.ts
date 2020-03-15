29f95698c224c044932c1f1e31037bd011b368e0
Marwa Gaser <marwagaser@gmail.com>
2020-03-16 00:41:13 +0200

    feat: added gallery and demo

--- src/app/photos-page/photos-page.component.ts
+++ src/app/photos-page/photos-page.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-photos-page',
-  templateUrl: './photos-page.component.html',
-  styleUrls: ['./photos-page.component.css']
+  selector: "app-photos-page",
+  templateUrl: "./photos-page.component.html",
+  styleUrls: ["./photos-page.component.css"]
 })
 export class PhotosPageComponent implements OnInit {
-
-  constructor() { }
+  url1 = "";
+  url2 = "";
+  url3 = "";
+  constructor() {}
 
   ngOnInit() {
+    this.url1 = "assets/url1.jpg";
+    this.url2 = "assets/url2.JPG";
+    this.url3 = "assets/url3.JPG";
   }
-
 }
